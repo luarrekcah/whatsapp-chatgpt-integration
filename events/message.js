d4f9e3ac4f670b944ecdbfe5ab11fcacb0bb07e6
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-15 20:11:18 -0500

    fixesss

--- events/message.js
+++ events/message.js
@@ -9,66 +9,89 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const autorizedGroups = ['553198585952-1571260307@g.us'];
+const autorizedGroups = ["553198585952-1571260307@g.us"];
 
 module.exports.run = async (client, message) => {
+  if (message.body !== undefined) {
+    if (message.isGroupMsg && !autorizedGroups.includes(message.from)) return;
+    if (
+      message.type === "ptt" ||
+      message.type === "video" ||
+      message.type === "image"
+    )
+      return;
+    if (message.body.length > 1000 || message.body.length <= 5) return;
+    const number = db.find((n) => n.number === message.from);
+    if (!number) {
+      client.sendText(
+        message.from,
+        "Um momento, vou te adicionar na minha lista de pessoas autorizadas... Aguarde 5 segundos e tente novamente."
+      );
+      db.push({ number: message.from, messages: "" });
+      const toStringData = JSON.stringify(db);
+      fs.writeFileSync(`${__dirname}/../db.json`, toStringData);
+      return;
+    }
     console.log(message);
-    if (message.body !== undefined) {
-        if(message.isGroupMsg && !autorizedGroups.includes(message.from)) return;
-        if(message.type === 'ptt' || message.type === 'video' || message.type === 'image') return
-        if(message.body.length > 1000 || message.body.length <= 5) return;
-        const number = db.find((n) => n.number === message.from);
-        if (!number) {
-          client.sendText(
-            message.from,
-            "Um momento, vou te adicionar na minha lista de pessoas autorizadas... Aguarde 5 segundos e tente novamente."
-          );
-          db.push({number: message.from, messages: ''})
-          const toStringData = JSON.stringify(db);
-          fs.writeFileSync(`${__dirname}/db.json`, toStringData);
-          return;
-        }
-        console.log(message.body);
-        let text = (number.messages += "\n\nHuman: " + message.body);
-        const completion = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt: `Human: ${message.body}`,
-          temperature: 0.9,
-          max_tokens: 150,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0.6,
-          stop: [" Human:", " AI:"],
-        });
-  
-        text += completion.data.choices[0].text;
-        const updateJson = db.map((i) => {
-          if (i.number === message.from) {
-            i.messages = text;
-          }
-          return i;
-        });
-  
-        const toStringData = JSON.stringify(updateJson);
-  
-        fs.writeFileSync(`${__dirname}/../db.json`, toStringData);
-  
-        client
-          .reply(
-            message.from,
-            completion.data.choices[0].text
-              .replace("\n", "")
-              .replace("\n", "")
-              .replace("Robot:", "")
-              .replace("Robô:", "")
-              .replace("Bot:", ""),
-              message.id
-          )
-          .then((result) => {
-            //console.log("Result: ", result);
-          })
-          .catch((erro) => {
-            console.error("Error when sending: ", erro);
-          });
+    let text = (number.messages += "\n\nHuman: " + message.body);
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Human: ${message.quotedMsg ? message.quotedMsg.body : ''} ${message.body}`,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
+    });
+
+    text += completion.data.choices[0].text;
+    const updateJson = db.map((i) => {
+      if (i.number === message.from) {
+        i.messages = text;
       }
-}
+      return i;
+    });
+
+    const toStringData = JSON.stringify(updateJson);
+
+    fs.writeFileSync(`${__dirname}/../db.json`, toStringData);
+
+    if (message.quotedMsg) {
+      client
+        .sendText(
+          message.from,
+          completion.data.choices[0].text
+            .replace("\n", "")
+            .replace("\n", "")
+            .replace("Robot:", "")
+            .replace("Robô:", "")
+            .replace("Bot:", "")
+        )
+        .then((result) => {
+          //console.log("Result: ", result);
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro);
+        });
+    } else {
+      client
+        .reply(
+          message.from,
+          completion.data.choices[0].text
+            .replace("\n", "")
+            .replace("\n", "")
+            .replace("Robot:", "")
+            .replace("Robô:", "")
+            .replace("Bot:", ""),
+          message.id
+        )
+        .then((result) => {
+          //console.log("Result: ", result);
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro);
+        });
+    }
+  }
+};
